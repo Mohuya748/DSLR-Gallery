4b455cdaf03eac56b80d56793d194b906ad3cdb2
Mohuya748 <khandakermohuya@gmail.com>
2022-04-05 23:25:51 +0600

    error fixed

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -9,7 +9,7 @@ const Reviews = () => {
             <h2 className='text-3xl text-green-700 font-bold'>Customer Reviews</h2>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5  mx-20 mt-10'>
                 {
-                    reviews.map(review => <Review review={review} key={reviews.id}></Review>)
+                    reviews.map(review => <Review review={review} key={review.id}></Review>)
                 }
 
             </div>
